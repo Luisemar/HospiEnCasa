90e2060933b7ba7ecb7e977b0e48853a3540a85b
unknown <luisemar@gmail.com>
2021-09-18 14:06:36 -0500

    fist commit

---
+++ HospiEnCasa.App.Consola/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using HospiEnCasa.App.Dominio;
+using HospiEnCasa.App.Persistencia;
+
+namespace HospiEnCasa.App.Consola
+{
+    class Program
+    {
+        private static IRepositorioPaciente _repoPaciente= new RepositorioPaciente(new Persistencia.AppContext());
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World EF!");
+            AddPaciente();
+        }
+        private static void AddPaciente()
+        {
+            var paciente = new Paciente
+            {
+                Nombre = "Luis",
+                Apellidos = "Matulanda",
+                NumeroTelefono = "3508197515",
+                Genero = Genero.Masculino,
+                Direccion = "Calle 25 # 57 a -25",
+                Longitud = 5.1221F,
+                Latitud = 75.525F,
+                Ciudad = "Bello"
+///                FechaNacimiento = New DateTime(1990, 04, 12)
+            };
+            _repoPaciente.AddPaciente(paciente);
+        }
+    }
+}
